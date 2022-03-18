c0c735fc830d9afc37b5b0482514bcc5a1aedace
MasonC <masoncong1@gmail.com>
2022-03-18 18:11:33 -0400

    baseline

---
+++ app/src/main/java/com/example/mangareaderapp/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.mangareaderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+
+public class SearchActivity {
+}
