3f3794c78b8312842dd6f53ce604c4573fcbde97
MasonC <masoncong1@gmail.com>
2022-03-18 18:11:33 -0400

    baseline

--- app/src/main/java/com/example/mangareaderapp/SearchActivity.java
+++ app/src/main/java/com/example/mangareaderapp/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.mangareaderapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.Group;
 
+<<<<<<< HEAD
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
@@ -56,4 +57,15 @@ public class SearchActivity extends AppCompatActivity {
 
     }
 
+=======
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+
+public class SearchActivity {
+>>>>>>> ff64a97 (baseline)
 }
