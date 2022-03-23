78d3c417bf2e39f927db5a4e8dfbca57a6bfb6ab
MasonC <masoncong1@gmail.com>
2022-03-23 19:02:11 -0400

    manga search

--- app/src/main/java/com/example/mangareaderapp/SearchActivity.java
+++ app/src/main/java/com/example/mangareaderapp/SearchActivity.java
@@ -7,10 +7,16 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.SearchView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -18,6 +24,14 @@ public class SearchActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_layout);
+        /*ListView searchDisplay = (ListView) findViewById(R.id.searchList);
+        ArrayList<String> mangas = new ArrayList<>();
+        mangas.add("a");
+        mangas.add("b");
+        mangas.add("c");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mangas);
+        searchDisplay.setAdapter(adapter);*/
 
         handleIntent(getIntent());
 
@@ -26,11 +40,19 @@ public class SearchActivity extends AppCompatActivity {
     private void handleIntent(Intent intent) {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            //doSearch(query);
+            doSearch(query);
         }
     }
 
-    private void doSearch() {
+    private void doSearch(String x) {
+        ListView searchDisplay = (ListView) findViewById(R.id.searchList);
+        ArrayList<String> mangas = new ArrayList<>();
+        mangas.add("a");
+        mangas.add("b");
+        mangas.add("c");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mangas);
+        searchDisplay.setAdapter(adapter);
 
     }
 
