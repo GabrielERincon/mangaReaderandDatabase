1b2a9dc6e725e0e406319b3c282d5c6d3708a15d
Gabriel Rincon <gabrielenrrincon@gmail.com>
2022-03-26 16:30:50 -0400

    Split Search Manga and implement tags

--- app/src/main/java/com/example/mangareaderapp/MangaDex.java
+++ app/src/main/java/com/example/mangareaderapp/MangaDex.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.Buffer;
 import java.nio.charset.StandardCharsets;
 import java.io.UnsupportedEncodingException;
 import java.nio.channels.Channels;
@@ -18,6 +19,7 @@ import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MangaDex {
     private int responseCode = -1;
@@ -27,7 +29,9 @@ public class MangaDex {
     private int apiPort = 443;
     private String dlHostname = "uploads.mangadex.org";
     private int dlPort = 443;
+    private Map<String, String> tags;
 
+    //Defines JsonKeys for usage in api calls
     enum Keys implements JsonKey {
         RESULT("result"),
         DATA("data"),
@@ -53,14 +57,18 @@ public class MangaDex {
         }
     }
 
+    //No paramater contructor
     public MangaDex() {
+        getTagInfo();
     }
 
+    //Construtor in the case of defining separate host names. Shouldn't be used for this project
     public MangaDex(String apiHostname, String dlHostname) {
         this.apiHostname = apiHostname;
         this.dlHostname = dlHostname;
     }
 
+    //Same as previous but with explicit ports as well
     public MangaDex(String apiHostname, int apiPort, String dlHostname, int dlPort) {
         this.apiHostname = apiHostname;
         this.apiPort = apiPort;
@@ -68,16 +76,80 @@ public class MangaDex {
         this.dlPort = dlPort;
     }
 
-    public List<Manga> searchManga(String pattern) {
+    /*Populates the tag map. Called in the contructor so you shouldn't need to call it unless
+    to refresh list*/
+    public void getTagInfo(){
+        tags = new HashMap<String, String>();
         URL url;
         HttpURLConnection con;
-        pattern.replace(" ", "-");
-        /* TODO: This needs to loop over the json responses and if needed ask for more
-           entries using the returned offset.
-         */
+        StringBuilder queryString = new StringBuilder("/manga/tag");
+
         try {
-            url = new URL("https", this.apiHostname, this.apiPort,
-                    "/manga?limit=50&title=" + URLEncoder.encode(pattern, "UTF-8"));
+            url = new URL("https", this.apiHostname, this.apiPort, queryString.toString());
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
+
+            if(con.getResponseCode() != 200){
+                throw new RuntimeException("Response code (getTags): " + con.getResponseCode());
+            } else {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                this.json = (JsonObject) Jsoner.deserialize(reader);
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        JsonArray data = json.getCollection(Keys.DATA);
+
+        for(Object dataItem : data){
+            Map<String, Map<String, Map<String, String>>> dataMap = (Map<String, Map<String, Map<String, String>>>) dataItem;
+            tags.put(dataMap.get("attributes").get("name").get("en"), (String) ((JsonObject) dataItem).get("id"));
+        }
+    }
+
+    //Get the tag map
+    public Map<String, String> getTags(){
+        return tags;
+    }
+
+    //Get list of manga by tag. Use the tag ids in the tag map
+    public List<Manga> searchByTag(String tagId){
+        return searchManga(null, tagId);
+    }
+
+    /*Get list of generic manga. These will literally be the first that the api returns, I don't
+    know what determines this list.*/
+    public List<Manga> searchManga() {
+        return searchManga(null, null);
+    }
+
+    //Get list of manga by title fitting the provided pattern
+    public List<Manga> searchManga(String pattern) {
+        return searchManga(pattern, null);
+    }
+
+    //Back bone of the search manga line. Use if you need to specify pattern and tag
+    public List<Manga> searchManga(String pattern, String tagId) {
+        URL url;
+        HttpURLConnection con;
+        StringBuilder queryString = new StringBuilder("/manga");
+
+        try {
+            pattern = URLEncoder.encode(pattern, "UTF-8");
+
+            if(pattern != null && tagId != null){
+                queryString.append("?title=" + pattern + "&includedTags[]=" + tagId);
+            } else if(pattern != null){
+                queryString.append("?title=" + pattern);
+            } else if(tagId != null){
+                queryString.append("?includedTags[]=" + tagId);
+            }
+
+            /* TODO: This needs to loop over the json responses and if needed ask for more
+            entries using the returned offset.
+            */
+            url = new URL("https", this.apiHostname, this.apiPort, queryString.toString());
             //System.out.println("URL: " + url);
 
             con = (HttpURLConnection) url.openConnection();
@@ -106,6 +178,8 @@ public class MangaDex {
         return mangas;
     }
 
+    /*For every manga in the list of mangas, it gets the available covers. See TestCovers
+    in the JUnit tests to see how to retrieve a cover.*/
     public void getCoverInfo(List<Manga> mangas) {
         URL url;
         HttpURLConnection con;
@@ -149,6 +223,7 @@ public class MangaDex {
         }
     }
 
+    //Populates the chapter list for the provided manga
     public void getChapterInfo(Manga manga){
         URL url;
         HttpURLConnection con;
@@ -187,6 +262,8 @@ public class MangaDex {
         } while(json.getInteger(Keys.TOTAL) > json.getInteger(Keys.LIMIT) + json.getInteger(Keys.OFFSET));
     }
 
+    /*Populates the pages list for the provided chapter. Note that the list is the file name for the
+    page*/
     public void getPagesInfo(MangaChapter chapter){
         URL url;
         HttpURLConnection con;
@@ -222,6 +299,7 @@ public class MangaDex {
         }
     }
 
+    //This is how you get the Cover. See TestCovers.
     public ReadableByteChannel streamCover(MangaCover cover) {
         StringBuilder queryString;
         ReadableByteChannel readChannel;
@@ -241,6 +319,7 @@ public class MangaDex {
         return readChannel;
     }
 
+    //Getting a page, see TestPage
     public ReadableByteChannel streamPage(MangaChapter chapter, String page) {
         StringBuilder queryString;
         ReadableByteChannel readChannel;
